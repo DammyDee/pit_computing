cc16a5d660823aed41fda9d1db9be4d282c16c88
DammyDee <damola.adewunmi@gmail.com>
2025-11-25 12:26:54 +0100

    Made changes to model to ensure password is required. Also made migrations and migrated the changes to the database.

--- payeproject/payeregistration/models.py
+++ payeproject/payeregistration/models.py
@@ -8,7 +8,7 @@ class PAYEAgent(models.Model):
     contact_email = models.EmailField(unique=True)
     contact_phone = models.CharField(max_length=20)
     address = models.TextField()
-    agent_password = models.CharField(default='', max_length=128)
+    agent_password = models.CharField(max_length=128)
     registration_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

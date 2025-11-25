3dc52f7cde134b38ed158cf0fdba029edae06ec2
DammyDee <damola.adewunmi@gmail.com>
2025-11-26 00:06:06 +0100

    fixed indenting problem

--- payeproject/payeregistration/forms.py
+++ payeproject/payeregistration/forms.py
@@ -10,11 +10,11 @@ class PAYEAgentForm(forms.ModelForm):
             'agent_password': forms.PasswordInput(),
         }
 
-        def clean(self):
-            cleaned_data = super().clean()
-            password = cleaned_data.get("agent_password")
-            confirm_password = cleaned_data.get("confirm_password")
-
-            if password != confirm_password:
-                raise forms.ValidationError("Password and Confirm Password do not match.")
-            return cleaned_data
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("agent_password")
+        confirm_password = cleaned_data.get("confirm_password")
+        
+        if password != confirm_password:
+            raise forms.ValidationError("Password and Confirm Password do not match.")
+        return cleaned_data

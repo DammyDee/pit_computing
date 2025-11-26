dbd0f214b77348baa95a52ef4b83c00502a0bad2
DammyDee <damola.adewunmi@gmail.com>
2025-11-27 00:11:44 +0100

    TODO: work on custom authentication based on model i created. Via the authentication, the payeagent will get acces to another page yet to be created.

--- payeproject/payecomputation/views.py
+++ payeproject/payecomputation/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})

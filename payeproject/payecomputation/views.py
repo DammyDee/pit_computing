1ed7285a659e4b79306095b2e8b799d762d6b043
DammyDee <damola.adewunmi@gmail.com>
2025-11-27 18:19:51 +0100

    Editted login.html template and views.py (in the payecomputation app). TODO: handle post request and authentication, work on payecomputation template, create and register a payecomputation model, make migrations anf migrate data. Possibly createpaye taxpayer model.

--- payeproject/payecomputation/views.py
+++ payeproject/payecomputation/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from payeregistration.models import PAYEAgent
+
 
 def login_view(request):
     if request.method == 'POST':
-        pass
+        agent_id = request.POST.get('id')
+        password = request.POST.get('password')
+        if agent_id and password:
+            try:
+                agent = PAYEAgent.objects.get(payer_id=agent_id)
+                if agent.check_password(password):
+                    # Successful login
+                    return redirect('dashboard')  # Redirect to a dashboard or home page
+                else:
+                    error_message = "Invalid password."
+            except PAYEAgent.DoesNotExist:
+                error_message = "Agent ID does not exist."
     else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+        error_message = None
+    return render(request, 'login.html')

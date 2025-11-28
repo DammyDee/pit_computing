ed0862f745640a4f54c762d4ea5dc6bba5055804
DammyDee <damola.adewunmi@gmail.com>
2025-11-28 16:13:23 +0100

    created payedashboard template. Added a model PayeComputation and registered it in admin.py

--- payeproject/payecomputation/models.py
+++ payeproject/payecomputation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class PayeComputation(models.Model):
+    employee_id = models.CharField(max_length=20)
+    employee_name = models.CharField(max_length=100)
+    gross_income = models.DecimalField(max_digits=10, decimal_places=2)
+    paye_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    computation_date = models.DateField(auto_now_add=True)
+    tax_year = models.CharField(max_length=9)  # e.g., "2023-2024"
+
+    
